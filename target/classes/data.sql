edef0dd03eac924f6140406e8985e9939796f24d
manmeetdevgun <manmeet.devgun@nagarro.com>
2021-02-18 23:28:37 +0530

    update gitignore

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO USER (`name`, `email`, `type`,`contact`, `city`, `state`, `pincode`, `wallet_balance`, `is_premium`) 
+			VALUES ('Manmeet Devgun', 'manmeet.devgun@nagarro.com', 'Regular', '9015035109','New Delhi','Delhi','110019', 1000.0,0);
