374c390936254cd4fe919e98ddaabe248b2c61c1
superuserz <manmeet.devgun@yahoo.in>
2021-02-26 14:35:55 +0530

    updated

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO USER (`name`, `email`, `type`,`contact`, `city`, `state`, `pincode`, `wallet_balance`, `is_premium`) 
-			VALUES ('Manmeet Devgun', 'manmeet.devgun@nagarro.com', 'REGULAR', '9015035109','New Delhi','Delhi','110019', 1000.0,0);
-
-INSERT INTO USER (`name`, `email`, `type`,`contact`, `city`, `state`, `pincode`, `wallet_balance`, `is_premium`) 
-			VALUES ('RAJESH KUMAR', 'manmeet.devgun@nagarro.com', 'PROVIDER', '9015035109','New Delhi','Delhi','110019', 1500.0,0);
